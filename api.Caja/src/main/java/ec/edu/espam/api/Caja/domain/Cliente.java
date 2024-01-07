6134265c5844a81c0ee2d1a178476faee71cba0a
Miriam Lectong <mirylectong18@gmail.com>
2024-01-07 10:56:04 -0500

    Project Inicialization

---
+++ api.Caja/src/main/java/ec/edu/espam/api/Caja/domain/Cliente.java
@@ -0,0 +1,7 @@
+package ec.edu.espam.api.Caja.domain;
+
+public class Cliente extends Persona {
+    private long password;
+    private String state;
+
+}
---
+++ api.Caja/src/main/java/ec/edu/espam/api/Caja/domain/Persona.java
@@ -0,0 +1,12 @@
+package ec.edu.espam.api.Caja.domain;
+
+
+public class Persona {
+    private long id;
+    private String identifier;
+    private String name;
+    private String gender;
+    private String direction;
+    private String phone;
+    private int age;
+}
